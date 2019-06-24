9a63f937c6818f16dcbc7bd00a5a26a46dd1eb49
koheihowniitsu <koheihowniitsu@howtelevision.jp>
2019-06-24 20:23:43 +0900

    temporary commit

--- RailsApp/app/controllers/people_controller.rb
+++ RailsApp/app/controllers/people_controller.rb
@@ -23,8 +23,13 @@ class PeopleController < ApplicationController
       redirect_to '/people'
   end
 
+  # ここで上記のperson_paramsの引数を決定している
   private
   def person_params
+    # requireはparamsクラスの中にあるメソットでparamsの中に指定の値が存在することをチェックするメソッド
+    # require(:person)ならparams[:person]という値があることをチェックしている
+    # permitというメソッドは引数に指定されている項目が用意されているかチェックするメソッド、:name,:age,:mailという項目があるかどうかをチェックしていた。
+    # これで安心してcreateに引数を渡せてめでたしめでたし。
     params.require(:person).permit(:name,:age,:mail)
   end
 
